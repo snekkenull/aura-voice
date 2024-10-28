733662f99a98ad66385802828cfd30a5e1a378ed
Vila <124278082+snekkenull@users.noreply.github.com>
2024-10-28 21:11:51 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const mode = process.env.BUILD_MODE ?? "standalone";
+console.log("[Next] build mode", mode);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: mode,
   images: {
     unoptimized: true,
   },
 };
+
+
 
 module.exports = nextConfig;
